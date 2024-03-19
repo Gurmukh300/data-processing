92bba4fa20b2aa0d84987f4141fc2ba5a4ed4572
Gurmukh300 <gurmukhs300@gmail.com>
2024-03-19 12:21:26 +1100

    csv file can be uploaded to the django and using pandas data type

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,37 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
-const krl = process.env.REACT_APP_REST_API_URL
+const krl = process.env.REACT_APP_REST_API_URL;
 function App() {
   const [datas, setData] = useState([]);
+  const [file, setFile] = useState(null);
 
   const fetchData = async () => {
-   
     try {
       const result = await axios.get(`${krl}`);
-   
-      setData(result.data)
-      
+
+      setData(result.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  const postData = async () => {
-    const name = "test z"
-    const birthdate = "2000-09-09"
-    const score = 5
-    const grade = "L"
-    const body = {name,birthdate,score,grade}
-    const response = await axios.post(krl, body)
-    console.log(response)
-    return response.data
-  }
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-  const handleSendData = async()=> {
-    const newData = await postData()
-    if (newData) {
-      setData(prevState => [...prevState, newData])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post(`${krl}upload_csv/`, formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error uploading file:", error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -42,18 +40,23 @@ function App() {
 
   return (
     <>
-     <div className="App">
-       {datas.length > 0 &&
-         datas.map((item, index) => (
-           <div key={index}>
-             {/* Render each property of the item */}
-             <p>ID: {item.id}</p>
-             <p>Name: {item.name}</p>
-             {/* Add more properties as needed */}
-           </div>
-         ))}
-      <button onClick={handleSendData}>Send</button>
-     </div>
+      <div className="App">
+        <br/>
+        <form onSubmit={handleSubmit}>
+          <input type="file" onChange={handleFileChange} />
+          <button type="submit">Upload</button>
+        </form>
+        {datas.length > 0 &&
+          datas.map((item, index) => (
+            <div key={index}>
+              {/* Render each property of the item */}
+              <p>ID: {item.id}</p>
+              <p>Name: {item.name}</p>
+              {/* Add more properties as needed */}
+            </div>
+          ))}
+        {/* <button onClick={handleSendData}>Send</button> */}
+      </div>
     </>
   );
 }
